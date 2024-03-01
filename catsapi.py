847825603a3d3e46b4d9f0d58d4fc20e7866c11a
Tyler Marcott <tylerjmarcott@gmail.com>
2024-03-01 10:28:04 -0700

    successful response from cat api

--- catsapi.py
+++ catsapi.py
@@ -1,2 +1,12 @@
 import requests
 
+url = "https://cdn2.thecatapi.com/images/ebv.jpg"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    data = response.json
+    print("Data retrieved successfully:", data)
+else:
+    print("failed retrieval of data. Status code:", response.status_code)
